46dde0425a4bd31025caf9da263ad42404c53fd4
manar <manarsayed1994@gmail.com>
2021-03-23 19:59:55 +0200

    feat: AdminBro

--- routes/admin.router.js
+++ routes/admin.router.js
@@ -6,9 +6,16 @@ const AdminBroMongoose = require('@admin-bro/mongoose');
 const mongoose = require('mongoose')
 AdminBro.registerAdapter(AdminBroMongoose)
 
+
+
 const adminBro = new AdminBro({
   databases: [mongoose],
   rootPath: '/admin',
+  branding: {
+    logo: 'https://pbs.twimg.com/profile_images/595898041929981952/qBVIEatN_400x400.png',
+    companyName: 'ShoseHouse',
+    softwareBrothers: false   // if Software Brothers logos should be shown in the sidebar footer
+  }
 })
 
 const router = AdminBroExpress.buildRouter(adminBro);
