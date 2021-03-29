29a4db6faf829556655161384dd829bdd712d869
manar <manarsayed1994@gmail.com>
2021-03-29 22:06:26 +0200

    feat: logo

--- routes/admin.router.js
+++ routes/admin.router.js
@@ -12,7 +12,7 @@ const adminBro = new AdminBro({
   databases: [mongoose],
   rootPath: '/admin',
   branding: {
-    logo: 'https://imgur.com/a/RKwDxSs',
+    logo: 'https://i.imgur.com/9OozRlA.jpeg',
     companyName: 'Lady Fashion',
     softwareBrothers: false   // if Software Brothers logos should be shown in the sidebar footer
   }
