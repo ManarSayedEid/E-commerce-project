b5d438c479c6c5ca00a3d29f47ed83eceb70f53b
manar <manarsayed1994@gmail.com>
2021-03-30 14:56:22 +0200

    feat: photo upload

--- connection.js
+++ connection.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 
 // process.env.MONGODB_URL || 'mongodb+srv://manar:manar@shoppingcart.n3vuw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority' 
 // || 
-mongoose.connect( 'mongodb://localhost:27017/shopping',
+mongoose.connect( process.env.MONGODB_URL || 'mongodb+srv://manar:manar@shoppingcart.n3vuw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority' 
+ ||'mongodb://localhost:27017/shopping',
  {useNewUrlParser: true, useUnifiedTopology: true} , (err) => {
      if (err){
          console.log('Failed to connect to MongDB');
